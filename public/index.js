d3536b177b46fa676083c6598c16c931005fb9e7
jackomonkey <bwallsisaboss@gmail.com>
2024-06-07 15:55:16 -0500

    Added new html pages, photos, and dynamic animations.

--- public/index.js
+++ public/index.js
@@ -1,7 +1,14 @@
+
+const initApp = () => {
 const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+const menu = document.querySelector('#mobile-menu');
 
 burger.addEventListener('click', () => {
-    document.querySelector('#menu').classList.toggle('hidden');
-    //document.querySelector('#burger').classList.toggle('hidden');
+    burger.classList.toggle('toggle-btn');
+    menu.classList.toggle('animate-open-menu');
+    menu.classList.toggle('opacity-100');
+
 })
+
+}
+document.addEventListener('DOMContentLoaded', initApp)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: ["./public/*.html","./public/*.js"],
   theme: {
     extend: {
       colors: {
@@ -8,10 +8,21 @@ module.exports = {
       },
 
       fontFamily: {
-        body: ['Nunito'],
-        jakarta: ['Plus Jakarta Sans']
+        jakarta: ['Plus Jakarta Sans'],
+        garamond: ['EB Garamond'],
+        roboto: ['Roboto']
+        
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': {transform: 'scaleY(0)'},
+          '100%': {transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.4s ease-out forwards',
+        'close-menu': 'open-menu 0.4s ease-out reverse'
       }
-
     },
   },
   plugins: [],
